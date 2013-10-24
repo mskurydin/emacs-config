10af7641aef8e517ae524527ee72391226c4b9fd
Maksim Skurydzin <mskurydin@gmail.com>
2013-10-25 01:06:31 +0300

    Some appearance customizations
    - Removed all extra UI components to get a distraction free mode
    - Added the current data and time to the mode panel

--- rc/emacs-rc-common.el
+++ rc/emacs-rc-common.el
@@ -1,12 +1,16 @@
+;;
+;; General emacs settings
+;; Some of the settings might be eventually moved to other configuration files
+;;
 
-;; No startup message
-(setq inhibit-startup-message t)
 
 ;; Enable the line numbers
 (global-linum-mode t)
 
-;; Show line and column numbers in the mode line
+;; Show line number in the mode line
 (line-number-mode 1)
+
+;; Show column number in the mode line
 (column-number-mode 1)
 
 ;; Highlight the current line
@@ -15,27 +19,35 @@
 ;; Highlight matching parathesis
 (show-paren-mode 1)
 
+
+;; No toolbar
+(tool-bar-mode 0)
+
+;; No menu
+(menu-bar-mode 0)
+
+;; No scrollbar
+(scroll-bar-mode 0)
+
+;; No emnacs startup message
+(setq inhibit-startup-message t)
+
+
+;; Display time in mode line
+(display-time-mode 1)
+(setq display-time-format "%H:%M [%a-%d/%m/%y]")
+
 ;;
 ;; TODO: Font settings should be put separately
-;;
-;; Font settings
 (set-face-attribute 'default nil :height 130)
 
 
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 3) 
 
-
-
-;;
-;; TOOD: where does it belong?
-;;
 ;; Emacs startup frame location and size
-
 (add-to-list 'default-frame-alist '(left . 0))
 (add-to-list 'default-frame-alist '(top . 0))
-
 (add-to-list 'default-frame-alist '(height . 70))
 (add-to-list 'default-frame-alist '(width . 90))
-
 
